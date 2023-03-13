e18d2e4c2937ee6e36cec1b8e4c606ff72eecf6f
djlemmor <lemmornuique@gmail.com>
2023-03-13 09:52:33 +0800

    Observer Pattern

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,12 +6,7 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-
-  const signOutUserHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  }
+  const { currentUser } = useContext(UserContext);
 
   return (
     <Fragment>
@@ -23,7 +18,7 @@ const Navigation = () => {
           <Link className="nav-link" to='/shop'>SHOP</Link>
           {
             currentUser ?
-              (<span className="nav-link" onClick={signOutUserHandler}>SIGN OUT</span>) :
+              (<span className="nav-link" onClick={signOutUser}>SIGN OUT</span>) :
               (<Link className="nav-link" to='/auth'>SIGN IN</Link>)
           }
         </div>
